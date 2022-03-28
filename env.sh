a6214d9aaa46a6672ceb0839f4f30fc9e3a34445
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-03-27 18:40:28 -0700

    remove refresh token check

--- env.sh
+++ env.sh
@@ -13,7 +13,6 @@ if [ -z $CERTBOT_EMAIL ] ; then err ; exit 1 ; fi
 
 if [ -z $CAPTCHA_SECRET ] ; then err ; exit 1 ; fi
 if [ -z $SECRET_ACCESS_TOKEN ] ; then err ; exit 1 ; fi
-if [ -z $SECRET_REFRESH_TOKEN ] ; then err ; exit 1 ; fi
 if [ -z $VITE_PROD_URL ] ; then err ; exit 1 ; fi
 if [ -z $VITE_RECAPTCHA_KEY ] ; then err ; exit 1 ; fi
 
